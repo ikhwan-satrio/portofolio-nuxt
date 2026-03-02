4ac2b52cf1f834d86d7aa07da72cf75bfbfc1fc2
wanto-production <ikwansatria3974@gmail.com>
2026-03-02 17:03:16 +0700

    update

--- app/app.vue
+++ app/app.vue
@@ -9,6 +9,10 @@ useHead({
       content:
         'portofolio,portofolio ikhwan,portofolio ikhwan satrio,ikhwan,ikhwan satrio,young dev portofolio',
     },
+    {
+      name: 'google-site-verification',
+      content: 'bWRaoFVlyB345Wietszb9IvK4x9MIN9hvtEA9ChIgoQ',
+    },
   ],
 });
 </script>
